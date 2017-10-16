54974c628637283af8fd8ad0d1b3015b027350c0
Uri Shaked <uri@salsa4fun.co.il>
2017-10-17 01:07:51 +0300

    fix: align code with the blog post

--- brain.js
+++ brain.js
@@ -3,23 +3,22 @@ require('rxjs/add/operator/map');
 
 const { MuseClient, channelNames } = require('muse-js');
 
-async function connect() {
+async function connectToMuse() {
     const client = new MuseClient();
     await client.connect();
-
-    const leftChannel = channelNames.indexOf('AF7');
-    const leftBlinks = client.eegReadings
+    await client.start();
+    
+    const leftChannel = channelNames.indexOf('AF7'); // Left eye electrode
+    const blinks = client.eegReadings
         .filter(r => r.electrode === leftChannel)
-        .map(r => Math.max(...r.samples.map(n => Math.abs(n))))
+        .map(r => Math.max(...r.samples.map(Math.abs)))
         .filter(max => max > 400);
 
-    client.start();
-
-    leftBlinks.subscribe(() => {
+    blinks.subscribe(() => {
         const jumpEvent = new Event('keydown');
-        jumpEvent.keyCode = 32;
+        jumpEvent.keyCode = 32; // Space key
         document.dispatchEvent(jumpEvent);
     });
 }
 
-window.connect = connect;
+window.connectToMuse = connectToMuse;
